5967765e499cac1b1684eebc551bedace005b3b5
sanjayimpact <sanjay.impactmindz@gmail.com>
2025-08-14 18:38:02 +0530

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port =process.env.PORT;
+console.log(port);
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
